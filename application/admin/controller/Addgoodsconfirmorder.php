30d67c6585adb6ab5dde26d2c3d590f69446b039
hjhsnail <hjhsnail@163.com>
2018-05-15 16:42:47 +0800

    hjh

--- application/admin/controller/Addgoodsconfirmorder.php
+++ application/admin/controller/Addgoodsconfirmorder.php
@@ -439,24 +439,6 @@ class Addgoodsconfirmorder extends Controller
         return $ret_info;
     }
 
-<<<<<<< HEAD
-    public function uploadfile(){
-        print_r('******************');
-        return;
-
-        if ($_FILES["file"]["error"] > 0) {
-            //echo "错误：: " . $_FILES["file"]["error"] . "<br>";
-        } else {
-            // 判断当期目录下的 upload 目录是否存在该文件
-            // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
-            if (file_exists("upload/" . $_FILES["file"]["name"])) {
-                //echo '0';
-            } else {
-                // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-                move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
-                //echo '1';
-            }
-=======
     /*上传咨询表*/
     public function uploadconsultform(){
         $file = request()->file('file');
@@ -473,7 +455,6 @@ class Addgoodsconfirmorder extends Controller
                 'msg'=>'上传失败',
             ];
             return json($res);
->>>>>>> 9317c036cacedcf31a04bb314af9ebdd2d38648e
         }
     }
 }
