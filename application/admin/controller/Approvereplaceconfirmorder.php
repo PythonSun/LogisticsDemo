81c4e1a92bcc79409b1e34f82c8fb11dcaa9f61e
hkm2018 <2249312321@qq.com>
2018-05-16 10:09:50 +0800

    hkm
    dfsfsds

--- application/admin/controller/Approvereplaceconfirmorder.php
+++ application/admin/controller/Approvereplaceconfirmorder.php
@@ -6,7 +6,25 @@ class Approvereplaceconfirmorder extends Controller
 {
 	/*新增订单渲染方法*/
     public function approvereplaceconfirmorder(){
+        $queryuserinfo = session("user_querypower");
+        $rolename = $queryuserinfo['role_name'];
+        if( $rolename == "管理人员" || $rolename == "部长/主管"||$rolename == "物流部人员")
+        {
+            $this->assign('current_user_type',5);
+        }
+        elseif ($rolename == "财务部")
+        {
+            $this->assign('current_user_type',4);
+        }
 
+        elseif($rolename == "总经理")
+        {
+                $this->assign('current_user_type',3);
+        }
+        elseif($rolename == "总监")
+        {
+            $this->assign('current_user_type',2);
+        }
         return $this->fetch();
     }
 
