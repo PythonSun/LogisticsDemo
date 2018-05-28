bbf8b683fdf82499f9b2a149d4a82e977ddeb742
hkm2018 <2249312321@qq.com>
2018-05-28 13:46:40 +0800

    hkm
    dfsf

--- application/admin/controller/Approvealternativeconfirmorder.php
+++ application/admin/controller/Approvealternativeconfirmorder.php
@@ -6,22 +6,54 @@ class Approvealternativeconfirmorder extends Controller
 {
 	/*审核订单渲染方法*/
     public function approvealternativeconfirmorder(){
+        $queryuserinfo = session("user_querypower");
+        $rolename = $queryuserinfo['role_name'];
+        if( $rolename == "管理人员" || $rolename == "部长/主管"||$rolename == "物流部人员")
+        {
+            $this->assign('current_user_type',5);
+        }
+        elseif ($rolename == "财务部")
+        {
+            $this->assign('current_user_type',4);
+        }
+
+        elseif($rolename == "总经理")
+        {
+            $this->assign('current_user_type',3);
+        }
+        elseif($rolename == "总监")
+        {
+            $this->assign('current_user_type',2);
+        }
     	return $this->fetch();
     }
 
     public function getexamineorder(){
-    	$page = $_GET['page'];
-    	$limit = $_GET['limit'];
+        $page = $_GET['page'];
+        $limit = $_GET['limit'];
         $user = session("user_session");
-    	$user_id = $user["user_id"];
-    	$orderType = 6;
-    	if(isset($_GET['queryInfo'])){
-    		$queryInfo = $_GET['queryInfo'];
-
-    		$tablelist = \app\index\model\Admin::queryApproveConfirmOrder($user_id,$orderType,$page,$limit,$queryInfo);
-    	}else{
-    		$tablelist = \app\index\model\Admin::queryApproveConfirmOrder($user_id,$orderType,$page,$limit);
-    	}
+        $user_id = $user["user_id"];
+        $orderType = 6;
+        $queryuserinfo = session("user_querypower");
+        $rolename = $queryuserinfo['role_name'];
+        if( $rolename == "管理人员" || $rolename == "部长/主管"||$rolename == "物流部人员")
+        {
+            if(isset($_GET['queryInfo'])){
+                $queryInfo = $_GET['queryInfo'];
+                $tablelist = \app\index\model\Admin::logisticQueryApproveConfirmOrder($orderType,$page,$limit,$queryInfo);
+            }else{
+                $tablelist = \app\index\model\Admin::logisticQueryApproveConfirmOrder($orderType,$page,$limit);
+            }
+        }
+        else
+        {
+            if(isset($_GET['queryInfo'])){
+                $queryInfo = $_GET['queryInfo'];
+                $tablelist = \app\index\model\Admin::queryApproveConfirmOrder($user_id,$orderType,$page,$limit,$queryInfo);
+            }else{
+                $tablelist = \app\index\model\Admin::queryApproveConfirmOrder($user_id,$orderType,$page,$limit);
+            }
+        }
         
     	return $tablelist;
     }
